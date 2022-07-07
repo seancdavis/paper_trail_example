9e8a8d649dcd5e9c15bb857e547597f100319702
Sean C Davis <scdavis41@gmail.com>
2022-07-06 20:43:23 -0400

    No tests

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,9 @@ module BasicExample
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework nil
+    end
   end
 end
