2e9faa94ca762ef5188fe01fcf8787ef4c61e094
Sean C Davis <scdavis41@gmail.com>
2022-07-19 06:45:56 -0400

    [WIP] Intro page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @posts = Post.all.order(created_at: :desc).limit(50).includes(:user)
