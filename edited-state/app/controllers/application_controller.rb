69f077be9c7a3d4f0d67ce7f9c1d6cc70430636a
Sean C Davis <scdavis41@gmail.com>
2022-08-12 07:16:26 -0400

    Remove method we're not using

--- edited-state/app/controllers/application_controller.rb
+++ edited-state/app/controllers/application_controller.rb
@@ -2,10 +2,4 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
-
-  private
-
-  def set_default_view_options
-    @view_options = { header: true }
-  end
 end
