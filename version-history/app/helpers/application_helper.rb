7b058d70eddb0620b2a2aad22bcad455ec0bea63
Sean C Davis <scdavis41@gmail.com>
2022-08-15 07:14:18 -0400

    Styles for feed page

--- version-history/app/helpers/application_helper.rb
+++ version-history/app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def secondary_button_classes
+    %w[
+      bg-stone-200
+      duration-300
+      flex
+      hover:bg-pink-500
+      hover:text-white
+      items-center
+      no-underline
+      p-2
+      rounded-md
+      text-inherit
+      transition-colors
+    ].join(' ')
+  end
 end
