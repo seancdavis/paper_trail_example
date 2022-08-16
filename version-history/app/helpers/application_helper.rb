7bf06e6181c3c178a2d89898b7db747f31b185a2
Sean C Davis <scdavis41@gmail.com>
2022-08-16 06:04:27 -0400

    Markdown renderer

--- version-history/app/helpers/application_helper.rb
+++ version-history/app/helpers/application_helper.rb
@@ -16,4 +16,9 @@ module ApplicationHelper
       transition-colors
     ].join(' ')
   end
+
+  def markdown(text)
+    options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks]
+    Markdown.new(text, *options).to_html.html_safe
+  end
 end
